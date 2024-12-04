39ff5d4f8e373ca16afbbf92f81ac786d217358c
Miss-Marce <90653079+Miss-Marce@users.noreply.github.com>
2024-12-03 22:16:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por tu mensaje! Me pondré en contacto contigo pronto.');
+});
